bb2d60f2c49fabc61d4a492b9d04f57b43a6af10
Darcy Liu <tearnon@live.com>
2014-03-15 21:01:09 +0800

    Solution for Length of Last Word

---
+++ length-of-last-word.cpp
@@ -0,0 +1,35 @@
+/*
+Solution for LeetCode problem
+
+Title: Length of Last Word
+URL: http://oj.leetcode.com/problems/length-of-last-word/
+Version: 1.0
+
+Created by Darcy Liu on 3/15/14.
+*/
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(const char *s) {
+        int length = 0,last = 0;
+        while (*s){
+            if (*s == ' '){
+                last = length>0?length:last;
+                length = 0;
+            }else{
+                length ++;
+            }
+            ++s;
+        }
+        return length>0?length:last;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout<< solution.lengthOfLastWord("Hello World")<<endl;
+    return 0;
+}
