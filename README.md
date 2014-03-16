3ea06432aef36904aad1324e8eee414057ae318f
Darcy Liu <tearnon@live.com>
2014-03-16 19:21:12 +0800

    Solution for Maximum Depth of Binary Tree

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 LeetCode
 ========
 
-Source code for http://oj.leetcode.com/
+Solution for  http://oj.leetcode.com/
+
+##Compiler(clang)
+
+	Apple LLVM version 5.0 (clang-500.2.79) (based on LLVM 3.3svn)
+	Target: x86_64-apple-darwin12.5.0
+	Thread model: posix
+
+## License
+
+The MIT License (MIT)
---
+++ maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,43 @@
+/*
+Solution for LeetCode problem
+
+Title: Maximum Depth of Binary Tree 
+URL: http://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+Version: 1.0
+
+Created by Darcy Liu on 3/16/14.
+*/
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+	public:
+	int maxDepth(TreeNode *root) {
+		if (root == NULL){
+			return 0;
+		}
+		int left = this->maxDepth(root->left);
+		int right = this->maxDepth(root->right);
+    	return left>right? left+1:right+1;
+	}
+};
+
+int main()
+{
+	TreeNode a1(1),a2(2),a3(3),a4(4),a5(5);
+	a1.left = &a2;
+	a2.right = &a3;
+	a3.left = &a4;
+	a3.right = &a5;
+			
+	Solution solution;
+	cout<< solution.maxDepth(&a1)<<endl;
+	return 0;
+}
