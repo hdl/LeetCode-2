15ea6a3d22582f48e6cae03b550cf79d6b5457df
Darcy Liu <tearnon@live.com>
2014-03-17 13:31:03 +0800

    add source code template

---
+++ template.cpp
@@ -0,0 +1,26 @@
+/*
+Solution for LeetCode problem
+
+Title: 
+URL: 
+Version: 1.0
+
+Created by Darcy Liu on 3/17/14.
+*/
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int example(int x){
+		return x;
+    }
+};
+
+int main()
+{
+    Solution solution;
+	cout<< solution.example(100) <<endl;
+    return 0;
+}
