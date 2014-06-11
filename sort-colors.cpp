6d36bc0ae03cf643ef2699c6f3e6b3dadf1e15a2
Darcy Liu <tearnon@live.com>
2014-06-11 15:05:04 +0100

    Solution for Sort Colors

---
+++ sort-colors.cpp
@@ -0,0 +1,37 @@
+/*
+Solution for LeetCode problem
+
+Title: Sort Colors
+URL: https://oj.leetcode.com/problems/sort-colors/
+Version: 1.0
+
+Created by Darcy Liu on 3/17/14.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        for (int i = 0, j=0; i < n; i++) {
+            if (A[i] == 0) {
+                swap(A[i],A[j++]);
+            } else if (A[i] == 2) {
+                swap(A[i--],A[--n]);
+            }
+        } 
+    }
+};
+
+int main()
+{
+    Solution solution;
+    int colors[] = {0,1,2,0,1,2,0};
+    solution.sortColors(colors,7);
+    for (int i = 0; i < 7; ++i)
+    {
+        cout << colors[i] << endl;
+    }
+    return 0;
+}
