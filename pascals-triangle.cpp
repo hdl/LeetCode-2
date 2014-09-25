42969dfd19df7f639438c1350dbb6b03a02bd158
Darcy Liu <tearnon@live.com>
2014-09-25 12:13:47 +0800

    Update solution for Pascal's Triangle

--- pascals-triangle.cpp
+++ pascals-triangle.cpp
@@ -18,14 +18,9 @@ public:
         vector<vector<int> > triangle;
 
         for (int i=0;i< numRows;i++){
-            vector<int> line;
-            for(int j=0;j<i+1;j++) {
-                if (j == 0 || j == i) {
-                    line.push_back(1);
-                } else {
-                    int r = triangle[i-1][j-1] + triangle[i-1][j];
-                    line.push_back(r);
-                }
+            vector<int> line(i+1,1);
+            for(int j=1;j<i;j++) {
+                line[j] = triangle[i-1][j-1] + triangle[i-1][j];
             }
             triangle.push_back(line);
         }
